f828e010d4a5ef59c4f39da011eae76c25ae15a9
Linda Chang <lindachang186@gmail.com>
2018-06-11 20:47:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,16 +1,9 @@
 function shout(string) {
   return string.toUpperCase()
 };
-function whisper(string) {
-  return whisper.toLowerCase()
-};
 function logShout(string){
   console.log(string.toUpperCase())
 };
 function logWhisper(string){
   console.log(string.toLowerCase())
 };
-
-function sayHiToGrandma(string) {
-  if(string.toLowerCase === lowercase)
-    return ("I can't hear you")};
