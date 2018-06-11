bf2f5ab7f4a7930e829cec8c01ca58eebe90e9d7
Linda Chang <lindachang186@gmail.com>
2018-06-11 20:38:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function sayHiToGrandma(string) {
     console.log("I can't hear you")
   } else if (string.toUpperCase === uppercase) {
     console.log("YES INDEED!")
-  } else (string === "I love you, Grandma") {
+  } else (string === "I love you, Grandma"){
     console.log("I love you, too")
   }
 };
